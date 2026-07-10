37e538504a93dee1ba566dbbbfe9ef9a28beda2d
Jaqueline <ijaquelinegagon@gmail.com>
2026-07-10 15:43:47 -0600

    Funcionamiento de los botones

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -1,43 +1,93 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'; // <-- Importamos FormsModule para usar [(ngModel)]
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink], // <-- Agrégalo aquí
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterLink
+  ],
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
 export class About {
-  // Precios base y valores por defecto
-  precioBase: number = 850; // Costo del circuito base + Arduino + motores
 
-  // Opciones seleccionadas por el usuario
+  constructor(private http: HttpClient) {}
+
+  // Precio base
+  precioBase: number = 850;
+
+  // Datos de la cotización
   tamanoContenedor: string = 'chico';
   incluyeCamara: boolean = false;
   materialEstructura: string = 'plastico';
   cantidadDispositivos: number = 1;
 
-  // Función matemática que calcula el precio final en tiempo real
+  mostrarInputCorreo = false;
+  // Correo del cliente
+  correoUsuario: string = '';
+
+  // Calcula el total automáticamente
   get calcularTotal(): number {
+
     let total = this.precioBase;
 
-    // 1. Modificador por tamaño
-    if (this.tamanoContenedor === 'mediano') total += 200;
-    if (this.tamanoContenedor === 'grande') total += 450;
+    if (this.tamanoContenedor === 'mediano')
+      total += 200;
 
-    // 2. Modificador por sensores / cámara
-    if (this.incluyeCamara) total += 350;
+    if (this.tamanoContenedor === 'grande')
+      total += 450;
 
-    // 3. Modificador por material
-    if (this.materialEstructura === 'premium') total += 500;
+    if (this.incluyeCamara)
+      total += 350;
 
-    // Multiplicamos por la cantidad de PawFeeders que se quieran armar
+    if (this.materialEstructura === 'premium')
+      total += 500;
+
     return total * this.cantidadDispositivos;
   }
+
+  mostrarCampoCorreo() {
+  this.mostrarInputCorreo = true;
 }
 
+enviarCotizacion() {
 
+  if (!this.correoUsuario) {
+    alert("Ingresa un correo electrónico.");
+    return;
+  }
+
+  const datos = {
+    correo: this.correoUsuario,
+    contenedor: this.tamanoContenedor,
+    material: this.materialEstructura,
+    cantidad: this.cantidadDispositivos,
+    total: this.calcularTotal
+  };
+
+  this.http.post(
+    'https://localhost:7122/api/Cotizacion/enviar',
+    datos
+  ).subscribe({
+    next: () => {
+      alert("La cotización fue enviada correctamente.");
+
+      // Limpiar formulario
+      this.correoUsuario = '';
+      this.mostrarInputCorreo = false;
+    },
+    error: (error) => {
+      console.error(error);
+      alert("Error al enviar la cotización.");
+    }
+  });
+
+}
+
+}
